f5fa49be665f3f1c2b03f737b5a54243250f0c98
kore-a <66811132+kore-a@users.noreply.github.com>
2020-06-15 09:07:14 -0400

    Add files via upload

--- function_output.m
+++ function_output.m
@@ -1,6 +1,5 @@
 
 function[timeclock,x,y,xdot,ydot]= function_output(time,timestep,x,y) %time is in seconds, x & y is within 8x8.
-distance=0;
 count=1;
 xdot=[];
 ydot=[];
@@ -10,16 +9,15 @@ for i=0:timestep:time
     count=count+1;
     x=[x randi([0 8])];
     y=[y randi([0 8])];
-    distanc_x=distance+sqrt((x(count)-x(count-1))^2+(y(count)-y(count-1))^2);
-    distance_y
+    distance_x= dist(x);
+    distance_y= dist(y);
     xdot=distance_y/time; 
-    ydotdistance_x/time;
+    ydot=distance_x/time;
+end
+plot(x,y)
 end
 
-end
-% %% change velocity to time, make time steps 0.1 seconds, make time steps a variable that 
-% would pass to the function. 
-% condition: once the x and y are outside the 8*8 parameter change it back inside to 8*8.
-% use to the random number function scale, multiplied by by 0.1
-% xdot= change in location/time (for x)
+% %% 
+% add condition: once the x and y are outside the 8*8 parameter change it back inside to 8*8.
+% use the random number function scale, multiplied by by 0.1
 
